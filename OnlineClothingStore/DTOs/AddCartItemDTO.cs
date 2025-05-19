62319d8bb153c3fe9c6696666fe476a193ae1766
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-19 10:33:05 +0400

    add dto for adding cartitem

---
+++ OnlineClothingStore/DTOs/AddCartItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineClothingStore.DTOs
+{
+    public class AddCartItemDTO
+    {
+        public int ProductId { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public int Quantity { get; set; }
+    }
+}
