3898bf1e040ebf57dc33be1f374c89fcfb1bfcfc
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-19 10:27:19 +0400

    add models for cart

---
+++ OnlineClothingStore/Models/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineClothingStore.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductVariantId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
