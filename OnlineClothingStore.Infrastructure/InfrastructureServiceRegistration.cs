244b5ab779a44055160a47daffef0327f4d3c145
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-28 14:21:39 +0400

    add class for service registration specific to infrastructure project

---
+++ OnlineClothingStore.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnlineClothingStore.Application;
+using OnlineClothingStore.Application.Contracts.Infrastructure;
+using OnlineClothingStore.Infrastructure.Repositories;
+
+namespace OnlineClothingStore.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IDbConnectionFactory, SqlConnectionFactory>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            return services;
+        }
+    }
+}
