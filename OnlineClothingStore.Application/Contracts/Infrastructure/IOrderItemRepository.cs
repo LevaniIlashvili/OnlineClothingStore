1155cd684a46a466d0e0f0eadea19cccf586e76b
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-29 17:21:09 +0400

    add repository interfaces

---
+++ OnlineClothingStore.Application/Contracts/Infrastructure/IOrderItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnlineClothingStore.Domain.Entities;
+
+namespace OnlineClothingStore.Application.Contracts.Infrastructure
+{
+    public interface IOrderItemRepository
+    {
+        Task<OrderItem?> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<OrderItem>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<IEnumerable<OrderItem>> GetByOrderIdAsync(long orderId, CancellationToken cancellationToken = default);
+        Task<OrderItem> AddAsync(OrderItem orderItem, CancellationToken cancellationToken = default);
+        Task UpdateAsync(OrderItem orderItem, CancellationToken cancellationToken = default);
+        Task DeleteAsync(OrderItem orderItem, CancellationToken cancellationToken = default);
+    }
+}
