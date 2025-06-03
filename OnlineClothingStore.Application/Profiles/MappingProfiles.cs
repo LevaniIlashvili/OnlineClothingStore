0d7059deded27d733e7305973f7b37bea50132b7
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-03 16:29:32 +0400

    add mapping profiles

---
+++ OnlineClothingStore.Application/Profiles/MappingProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using OnlineClothingStore.Application.DTOs;
+using OnlineClothingStore.Application.Features.Brands.Commands.CreateBrand;
+using OnlineClothingStore.Application.Features.Categories.Commands.CreateCategory;
+using OnlineClothingStore.Application.Features.Products.Commands.CreateProduct;
+using OnlineClothingStore.Application.Features.Products.Commands.UpdateProduct;
+using OnlineClothingStore.Domain.Entities;
+
+namespace OnlineClothingStore.Application.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Category, CreateCategoryCommand>().ReverseMap();
+
+            CreateMap<Brand, BrandDTO>().ReverseMap();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Product, CreateProductCommand>().ReverseMap();
+            CreateMap<Product, UpdateProductCommand>().ReverseMap();
+        }
+    }
+}
