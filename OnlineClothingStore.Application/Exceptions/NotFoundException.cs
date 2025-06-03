283a58ed6bfda293c1a42ce866c7e6bfb4fb619c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-03 16:29:50 +0400

    create custom exceptions

---
+++ OnlineClothingStore.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineClothingStore.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
