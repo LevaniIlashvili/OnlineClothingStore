519bdd299e3855d04672e98ed10a82e2174b8bb5
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-03 16:30:11 +0400

    add dtos

---
+++ OnlineClothingStore.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineClothingStore.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public long? ParentCategoryId { get; set; }
+    }
+}
