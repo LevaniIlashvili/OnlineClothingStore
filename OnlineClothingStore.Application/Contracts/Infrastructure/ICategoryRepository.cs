adbc63f3dc5f96352f86fe6cba18a1a1c59c9663
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-03 16:41:44 +0400

    add getcategorybyname and haschildren methods

--- OnlineClothingStore.Application/Contracts/Infrastructure/ICategoryRepository.cs
+++ OnlineClothingStore.Application/Contracts/Infrastructure/ICategoryRepository.cs
@@ -6,6 +6,8 @@ namespace OnlineClothingStore.Application.Contracts.Infrastructure
     {
         Task<Category?> GetByIdAsync(long id, CancellationToken cancellationToken = default);
         Task<IEnumerable<Category>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<Category?> GetByNameAsync(string name, CancellationToken cancellationToken = default);
+        Task<bool> HasChildrenAsync(long id, CancellationToken cancellationToken = default);
         Task<Category> AddAsync(Category category, CancellationToken cancellationToken = default);
         Task UpdateAsync(Category category, CancellationToken cancellationToken = default);
         Task DeleteAsync(Category category, CancellationToken cancellationToken = default);
