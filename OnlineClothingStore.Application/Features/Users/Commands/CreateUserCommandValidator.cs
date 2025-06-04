c31150e3dba95e0d9fb2927797e49ddd301f1dea
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-04 22:01:26 +0400

    add functionality to create users

---
+++ OnlineClothingStore.Application/Features/Users/Commands/CreateUserCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace OnlineClothingStore.Application.Features.Users.Commands
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required")
+                .MaximumLength(50);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required")
+                .MaximumLength(50);
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long");
+
+            RuleFor(x => x.PhoneNumber)
+                .Matches(@"^\+?\d{10,15}$")
+                .When(x => !string.IsNullOrWhiteSpace(x.PhoneNumber))
+                .WithMessage("Phone number must be valid");
+        }
+    }
+}
