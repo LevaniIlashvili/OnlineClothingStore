69b6756f021f2174776759185eaeeb7db7fac759
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-06 15:37:59 +0400

    add query to login user

---
+++ OnlineClothingStore.Application/Features/Users/Queries/LoginUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace OnlineClothingStore.Application.Features.Users.Queries
+{
+    public class LoginUserQuery : IRequest<string>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
