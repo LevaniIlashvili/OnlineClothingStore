0b490cfe5ca395ec8782271971623d740aaa09e4
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-09 20:55:03 +0400

    use user secrets

--- OnlineClothingStore.Api/Program.cs
+++ OnlineClothingStore.Api/Program.cs
@@ -8,6 +8,11 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
 // Add services to the container.
 builder.Services.AddHttpContextAccessor();
 
