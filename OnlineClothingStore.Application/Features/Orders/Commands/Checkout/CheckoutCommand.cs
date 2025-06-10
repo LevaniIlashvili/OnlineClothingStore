8edda66c61c7f9592a1bb54f529b1eb4b48866fb
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-10 12:38:16 +0400

    add userid to command

--- OnlineClothingStore.Application/Features/Orders/Commands/Checkout/CheckoutCommand.cs
+++ OnlineClothingStore.Application/Features/Orders/Commands/Checkout/CheckoutCommand.cs
@@ -6,5 +6,6 @@ namespace OnlineClothingStore.Application.Features.Orders.Commands.Checkout
     public class CheckoutCommand : IRequest<OrderDTO>
     {
         public string ShippingAddress { get; set; }
+        public long UserId { get; set; }
     }
 }
